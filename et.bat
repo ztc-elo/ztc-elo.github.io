2b0b2d5137104e99ec029dc87f9521be326f01a6
Sámuel Fekete <feketesamu@gmail.com>
2022-06-30 20:18:20 +0200

    auto commit

--- et.bat
+++ et.bat
@@ -1,12 +1,13 @@
-out=$(iconv -f windows-1250 -t utf-8 et01_szar.html) && echo "$out" > et01.html
-out2=$(iconv -f windows-1250 -t utf-8 et02_szar.html) && echo "$out2" > et02.html
-out3=$(iconv -f windows-1250 -t utf-8 et03_szar.html) && echo "$out3" > et03.html
-out4=$(iconv -f windows-1250 -t utf-8 et04_szar.html) && echo "$out4" > et04.html
-out5=$(iconv -f windows-1250 -t utf-8 et05_szar.html) && echo "$out5" > et05.html
-out6=$(iconv -f windows-1250 -t utf-8 et06_szar.html) && echo "$out6" > et06.html
-out7=$(iconv -f windows-1250 -t utf-8 et07_szar.html) && echo "$out7" > et07.html
-out8=$(iconv -f windows-1250 -t utf-8 et08_szar.html) && echo "$out8" > et08.html
-out9=$(iconv -f windows-1250 -t utf-8 et09_szar.html) && echo "$out9" > et09.html
+iconv -f windows-1250 -t utf-8 et01_szar.html > et01.html
+iconv -f windows-1250 -t utf-8 et02_szar.html > et02.html
+iconv -f windows-1250 -t utf-8 et03_szar.html > et03.html
+iconv -f windows-1250 -t utf-8 et04_szar.html > et04.html
+iconv -f windows-1250 -t utf-8 et05_szar.html > et05.html
+iconv -f windows-1250 -t utf-8 et06_szar.html > et06.html
+iconv -f windows-1250 -t utf-8 et07_szar.html > et07.html
+iconv -f windows-1250 -t utf-8 et08_szar.html > et08.html
+iconv -f windows-1250 -t utf-8 et09_szar.html > et09.html
+for /r %%F in (*) do if %%~zF==0 del "%%F"
 git pull
 git add --all
 git commit -m "auto commit"
