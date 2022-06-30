320f8c79bc55c76790be36814b9f5ecb2b031987
Sámuel Fekete <feketesamu@gmail.com>
2022-06-30 20:11:20 +0200

    auto commit

--- et.bat
+++ et.bat
@@ -1,4 +1,4 @@
-iconv -f windows-1250 -t utf-8 et01_szar.html > et01.html
+out=$(iconv -f windows-1250 -t utf-8 et01_szar.html) && echo "$out" > et01.html
 git pull
 git add --all
 git commit -m "auto commit"
