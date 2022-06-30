c560c064f12409fae5231e12fb954064c6774a2b
Sámuel Fekete <feketesamu@gmail.com>
2022-06-30 22:00:32 +0200

    auto commit

--- et22/et.bat
+++ et22/et.bat
@@ -7,6 +7,7 @@ iconv -f windows-1250 -t utf-8 et06_szar.html > et06.html
 iconv -f windows-1250 -t utf-8 et07_szar.html > et07.html
 iconv -f windows-1250 -t utf-8 et08_szar.html > et08.html
 iconv -f windows-1250 -t utf-8 et09_szar.html > et09.html
+timeout 1
 for /r %%F in (*) do if %%~zF==0 del "%%F"
 git pull
 git add --all
